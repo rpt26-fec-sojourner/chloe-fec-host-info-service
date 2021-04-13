e508c4c7b67a565a0474484fb1d6a1287d271b41
Chloe Tan <chloe.tan95@gmail.com>
2021-04-13 00:21:03 -0400

    Add webpack config, necessary files

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 2222;
+const bodyParser = require('body-parser');
+const path = require('path');
+const distPath = path.join(__dirname, 'client/dist');
+
+app.use(express.static(distPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+//TO DO: GET host info API endpoint
+
+app.get('/:listing_id', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,7 @@
+
+const app = require('../app.js');
+const port = 2222;
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
